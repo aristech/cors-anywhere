1b9235911f3a254106c91c7ca75af58630d4234c
aristech <aris@progressnet.gr>
2025-01-30 12:29:55 +0200

    env

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
+require('dotenv').config();
+const cors_proxy = require("./lib/cors-anywhere");
+const rateLimit = require('./lib/rate-limit');
+
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-
-// Grab the blacklist from the command-line so that we can update the blacklist without deploying
-// again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
-// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
-// use originWhitelist instead.
-var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+// Function to parse environment variable lists
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -17,10 +15,14 @@ function parseEnvList(env) {
   return env.split(',');
 }
 
-// Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
-var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
+// Grab the blacklist and whitelist from the environment variables
+const originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
+const originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
 
-var cors_proxy = require('./lib/cors-anywhere');
+// Set up rate-limiting to avoid abuse of the public CORS Anywhere server
+const checkRateLimit = rateLimit(process.env.CORSANYWHERE_RATELIMIT);
+
+// Create and start the CORS Anywhere server
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
@@ -42,7 +44,7 @@ cors_proxy.createServer({
   ],
   redirectSameOrigin: true,
   httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it
     xfwd: false,
   },
 }).listen(port, host, function() {
